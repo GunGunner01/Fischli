4c6d86f858ed06aca699f2e1bb4f5407fa13e645
Tim Renggli <rengt2@bfh.ch>
2024-02-05 17:40:26 +0100

    bra

--- src/main/java/org/example/controller/VoteServlet.java
+++ src/main/java/org/example/controller/VoteServlet.java
@@ -6,10 +6,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.model.PollAdmin;
-import org.example.model.Vote;
+import org.example.model.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @WebServlet("/api/votes")
 public class VoteServlet extends HttpServlet {
@@ -18,41 +19,28 @@ public class VoteServlet extends HttpServlet {
     private static final ObjectMapper objectMapper = ObjectMapperFactory.createObjectMapper();
     private static final PollAdmin pollAdmin = PollAdmin.getInstance();
 
-//    @Override
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String poll_id = request.getParameter("poll_id");
-//        response.setStatus(HttpServletResponse.SC_OK);
-//        response.setContentType(JSON_MEDIA_TYPE);
-//        List<PollInfo> polls = pollAdmin.getPolls(poll_id);
-//        objectMapper.writeValue(response.getOutputStream(), polls);
-//    }
-
-
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
 
+            int poll_id = Integer.parseInt(request.getParameter("pollId"));
+            String option = request.getParameter("option");
 
+            Poll poll = pollAdmin.findPoll(poll_id);
 
+            LocalDate now = LocalDate.from(LocalDateTime.now());
 
+            if (now.isBefore(poll.getExpiration())) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                return;
+            }
 
-          String poll_id = request.getParameter("pollId");
-          String option = request.getParameter("option");
+            Vote vote = new Vote(poll_id, option);
 
-          Vote vote = new Vote(Integer.parseInt(poll_id), option);
+            poll.addVote(option);
 
-//            ChatMessage message = objectMapper.readValue(request.getInputStream(), ChatMessage.class);
-//            if (message.getId() != null || message.getText() == null || message.getText().isEmpty()) {
-//                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-//                return;
-//            }
-//            // add message
-//            logger.info("Adding message");
-//            chatService.addMessage(message);
-//            response.setStatus(HttpServletResponse.SC_CREATED);
-//            response.setContentType("application/json");
-//            objectMapper.writeValue(response.getOutputStream(), message);
-        } catch (JsonProcessingException ex) {
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (PollNotFoundException | InvalidVoteException ex) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
